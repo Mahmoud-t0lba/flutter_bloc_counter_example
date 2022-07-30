3f9b50e0ffb63e564ff0d3bd98f7be7303c695f4
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2022-07-30 13:15:29 +0200

    first commit

---
+++ lib/widgets/buttons_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_counter_example/bloc/counter_bloc.dart';
+
+class ButtonsWidget extends StatelessWidget {
+  const ButtonsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          onPressed: () {
+            BlocProvider.of<CounterBloc>(context).add(IncreamentEvent());
+            // context.read<CounterBloc>().add(IncreamentEvent());
+          },
+          child: const Icon(Icons.add),
+        ),
+        const SizedBox(height: 10),
+        FloatingActionButton(
+          onPressed: () {
+            BlocProvider.of<CounterBloc>(context).add(ResetEvent());
+            // context.read<CounterBloc>().add(ResetEvent());
+          },
+          child: const Icon(Icons.exposure_zero),
+        ),
+        const SizedBox(height: 10),
+        FloatingActionButton(
+          onPressed: () {
+            //  BlocProvider.of<CounterBloc>(context).add(DecreamentEvent());
+            context.read<CounterBloc>().add(DecreamentEvent());
+          },
+          child: const Icon(Icons.remove),
+        ),
+      ],
+    );
+  }
+}
